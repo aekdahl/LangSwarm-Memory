38dd03939b6638d13e59cba53af1ccccad2d1903
aekdahl <alexander.ekdahl@gmail.com>
2024-12-23 16:58:49 +0100

    Update in_memory.py

--- memswarm/in_memory.py
+++ memswarm/in_memory.py
@@ -7,16 +7,35 @@ except ImportError:
     cosine_similarity = None
     SentenceTransformer = None
 
+from threading import Lock
+from datetime import datetime
+            
 class InMemorySharedMemory(SharedMemoryBase):
     """
     A basic in-memory implementation of shared memory.
     """
 
-    def __init__(self):
+    def __init__(self, thread_safe = True):
         self.memory = {}
+        self.thread_safe = thread_safe
+        
+        if self.thread_safe:
+            self.lock = Lock()
+
         if SentenceTransformer:
             self.model = SentenceTransformer("all-MiniLM-L6-v2")
             self.embeddings = {}
+
+    def _get_default_metadata(self, metadata):
+        now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        return {
+            "agent": metadata.get("agent"),
+            "timestamp": metadata.get("timestamp", now),
+            "llm": metadata.get("llm"),
+            "action": metadata.get("action"),
+            "confidence": metadata.get("confidence"),
+            "query": metadata.get("query"),
+        }
 
     def read(self, key=None):
         """
@@ -28,31 +47,57 @@ class InMemorySharedMemory(SharedMemoryBase):
         Returns:
         - The value for the key if specified, else all memory as a dictionary.
         """
+        if self.thread_safe:
+            with self.lock:
+                if key:
+                    return self.memory.get(key)
+                return self.memory.copy()
+        else:
         if key:
             return self.memory.get(key)
         return self.memory.copy()
-
-    def write(self, key, value):
+            
+    def write(self, key, value, metadata=None):
         """
         Write a key-value pair to memory.
         """
-        self.memory[key] = value
-        if SentenceTransformer:
-            self.embeddings[key] = self.model.encode(value)
-
+        metadata = metadata or {}
+        _value = {
+            "value": value,
+            "metadata": self._get_default_metadata(metadata),
+        }
+        if self.thread_safe:
+            with self.lock:
+                self.memory[key] = _value
+                if SentenceTransformer:
+                    self.embeddings[key] = self.model.encode(_value)
+        else:
+            self.memory[key] = _value
+            if SentenceTransformer:
+                self.embeddings[key] = self.model.encode(_value)
+            
     def delete(self, key):
         """
         Delete a specific key from memory.
         """
-        if key in self.memory:
-            del self.memory[key]
-
+        if self.thread_safe:
+            with self.lock:
+                if key in self.memory:
+                    del self.memory[key]
+        else:
+            if key in self.memory:
+                del self.memory[key]
+                
     def clear(self):
         """
         Clear all memory.
         """
-        self.memory.clear()
-
+        if self.thread_safe:
+            with self.lock:
+                self.memory.clear()
+        else:
+            self.memory.clear()
+            
     def similarity_search(self, query, top_k=5):
         if cosine_similarity is not None and SentenceTransformer is ot None:
             query_vector = self.model.encode(query)
