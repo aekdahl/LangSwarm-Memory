232ad1559a80d72f20dd21a5ddb428f6bd3ff440
aekdahl <alexander.ekdahl@gmail.com>
2024-12-23 14:35:26 +0100

    Create redis_memory.py

---
+++ memswarm/redis_memory.py
@@ -0,0 +1,22 @@
+import redis
+from .base import SharedMemoryBase
+
+class RedisSharedMemory(SharedMemoryBase):
+    """
+    Redis-backed shared memory.
+    """
+
+    def __init__(self, redis_url="redis://localhost:6379/0"):
+        self.client = redis.StrictRedis.from_url(redis_url)
+
+    def read(self):
+        return {key.decode(): self.client.get(key).decode() for key in self.client.keys()}
+
+    def write(self, key, value):
+        self.client.set(key, value)
+
+    def delete(self, key):
+        self.client.delete(key)
+
+    def clear(self):
+        self.client.flushdb()
