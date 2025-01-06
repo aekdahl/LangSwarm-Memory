e1d22443384f2a7979acd9116992c205f75c30b3
aekdahl <alexander.ekdahl@gmail.com>
2025-01-06 14:23:32 +0100

    Update memory_manager.py

--- langswarm/memory/memory_manager.py
+++ langswarm/memory/memory_manager.py
@@ -1,17 +1,33 @@
 class MemoryManager:
-    def __init__(self, backend="langchain", **kwargs):
-        if backend == "langchain":
-            self.adapter = LangChainAdapter(**kwargs)
-        elif backend == "llama_index":
-            self.adapter = LlamaIndexAdapter(index_path=kwargs.get("index_path", "index.json"))
-        else:
-            raise ValueError("Unsupported backend")
+    def __init__(self, backends=None, **kwargs):
+        """
+        Initialize MemoryManager with multiple backends.
+
+        Args:
+            backends (list): List of backend configurations. Each entry can specify
+                            the backend type (e.g., "langchain", "llama_index") and
+                            corresponding parameters.
+        """
+        self.adapters = []
+        if backends:
+            for backend in backends:
+                if backend["type"] == "langchain":
+                    self.adapters.append(LangChainAdapter(**backend.get("params", {})))
+                elif backend["type"] == "llama_index":
+                    self.adapters.append(LlamaIndexAdapter(**backend.get("params", {})))
+                else:
+                    raise ValueError(f"Unsupported backend: {backend['type']}")
 
     def add_documents(self, documents):
-        self.adapter.add_documents(documents)
+        for adapter in self.adapters:
+            adapter.add_documents(documents)
 
     def query(self, query, filters=None):
-        return self.adapter.query(query, filters)
+        results = []
+        for adapter in self.adapters:
+            results.extend(adapter.query(query, filters))
+        return results
 
     def delete(self, document_ids):
-        self.adapter.delete(document_ids)
+        for adapter in self.adapters:
+            adapter.delete(document_ids)
