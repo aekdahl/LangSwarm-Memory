aa0152131662063183303e4c02eef3df7a42ce83
aekdahl <alexander.ekdahl@gmail.com>
2025-01-06 10:35:55 +0100

    Create llamaindex.py

---
+++ langswarm/memory/adapters/llamaindex.py
@@ -0,0 +1,28 @@
+from .database_adapter import DatabaseAdapter
+
+try:
+    from llama_index import GPTSimpleVectorIndex, Document
+except ImportError:
+    GPTSimpleVectorIndex = None
+    Document = None
+
+class LoadFromDiskAdapter(DatabaseAdapter):
+    def __init__(self, index_path="index.json"):
+        try:
+            self.index = GPTSimpleVectorIndex.load_from_disk(index_path)
+        except FileNotFoundError:
+            self.index = GPTSimpleVectorIndex([])
+
+    def add_documents(self, documents):
+        docs = [Document(text=doc["text"], metadata=doc.get("metadata", {})) for doc in documents]
+        self.index.insert(docs)
+        self.index.save_to_disk()
+
+    def query(self, query, filters=None):
+        results = self.index.query(query)
+        if filters:
+            results = [res for res in results if all(res.extra_info.get(k) == v for k, v in filters.items())]
+        return results
+
+    def delete(self, document_ids):
+        raise NotImplementedError("Delete functionality not implemented for LlamaIndex")
