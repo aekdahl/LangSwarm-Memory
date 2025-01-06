e8e4702d5986a617a95b2245844a92a979a8359f
aekdahl <alexander.ekdahl@gmail.com>
2025-01-06 10:28:13 +0100

    Create langchain.py

---
+++ langswarm/memory/adapters/langchain.py
@@ -0,0 +1,28 @@
+try:
+    from langchain.vectorstores import Pinecone
+    from langchain.embeddings.openai import OpenAIEmbeddings
+    import pinecone
+except ImportError:
+    Pinecone = None
+    OpenAIEmbeddings = None
+    pinecone = None
+
+class PineconeAdapter(DatabaseAdapter):
+    def __init__(self, *args, **kwargs):
+        if all(var is not None for var in (Pinecone, OpenAIEmbeddings, pinecone)):
+            pinecone.init(api_key=kwargs["api_key"], environment=kwargs["environment"])
+            self.db = Pinecone(index_name=kwargs["index_name"], embedding_function=OpenAIEmbeddings())
+        else:
+            raise ValueError("Unsupported vector database for LangChain")
+
+    def add_documents(self, documents):
+        texts = [doc["text"] for doc in documents]
+        metadata = [doc.get("metadata", {}) for doc in documents]
+        self.db.add_texts(texts, metadatas=metadata)
+
+    def query(self, query, filters=None):
+        return self.db.similarity_search(query, filter=filters)
+
+    def delete(self, document_ids):
+        for doc_id in document_ids:
+            self.db.delete(doc_id)
